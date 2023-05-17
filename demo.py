e5a7545608da854bf1c1286bc0643663b854af6e
tojahacko <an20.olimpicv2@gmail.com>
2023-05-17 17:06:24 +0200

    Initial commit

---
+++ demo.py
@@ -0,0 +1,20 @@
+# %%
+
+def fizz_buzz(i):
+    print(i)
+    3, 6, 9, 12 -> "fizz"
+    5, 10, 20 -> "buzz"
+    15, 30, 45, 60 -> "fizzbuzz"
+    i
+
+for i in range(1, 101):
+    fizz_buzz(i)
+# %%
+
+1,
+2,
+'fizz',
+4,
+'buzz'
+...
+(15) 'fizzbuzz'
